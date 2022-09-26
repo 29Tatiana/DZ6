ebe7d0f8570df8af5afaba8ebcd26445aa24ebc2
29Tatiana <amiravob@mail.ru>
2022-09-26 13:23:22 +0300

    задача41р

--- задача41/Program.cs
+++ задача41/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-Console.Write ("Введите любое количество чисел через пробел:");
-string line = Console.ReadLine ()!; 
-string[] parts = line.Split (' '); 
+Console.Write("Введите любое количество чисел через пробел:");
+string line = Console.ReadLine()!;
+string[] parts = line.Split(' ');
 int[] numbers = new int[parts.Length];
 
 for (int i = 0; i < numbers.Length; i++)
-numbers[i] = int.Parse (parts[i]);
+    numbers[i] = int.Parse(parts[i]);
 
 int CountPositiveNumbers(int[] numbers)
 {
@@ -14,7 +14,7 @@ int CountPositiveNumbers(int[] numbers)
     {
         if (numbers[i] > 0)
         {
-            count ++;
+            count++;
         }
     }
     return count;
